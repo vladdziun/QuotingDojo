920045f69478a4f90735b6747c4933cb7470e86e
unknown <v.dziun@gmail.com>
2019-04-08 17:11:19 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+using DbConnection;
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(User user)
+        {
+            string query = $"INSERT INTO users (name, quote) VALUES ('{user.Name}', '{user.Quote}');";
+            DbConnector.Execute(query);
+            // other code
+            return RedirectToAction("Result");
+        }
+        [Route("/result")]
+        [HttpGet]
+        public IActionResult Result()
+        {
+    	    List<Dictionary<string, object>> AllUsers = DbConnector.Query("SELECT * FROM users");
+            // To provide this data, we could use ViewBag or a View Model.  ViewBag shown here:
+            ViewBag.Users = AllUsers;
+            return View("Result");
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace QuotingDojo.Models
+{
+    public class User
+    {
+       public string Name {get; set; }
+       public string Quote {get; set; }
+    }
+}
